34276552a64e7cbe230d698930ff4570b27b6a14
Oleg <kharenkoleg@gmail.com>
2023-11-19 19:32:37 +0100

    a ver si chuta 1 & 2

--- DAM2_UF2_M06_A2_Premiere_League/src/main/java/TEST/ManagerTestInteractive.java
+++ DAM2_UF2_M06_A2_Premiere_League/src/main/java/TEST/ManagerTestInteractive.java
@@ -1,9 +1,22 @@
 package TEST;
 
+import DAO.DAOManager;
+import DAO.DAOManagerFactory;
+
 public class ManagerTestInteractive {
 
 	public static void main(String[] args) {
 		
 		// Here I'll do the test for all methods
+		final String CLUBS_FILE = "clubs.csv";
+		final String RESULTS_FILE = "results.csv";
+		
+		
+		DAOManagerFactory daoFac = new DAOManagerFactory();
+		DAOManager dao = daoFac.createDAOManager();
+		
+		dao.ImportTeams(CLUBS_FILE);
+
+
 	}
 }
