689e846896bed3acbd320b743c9991dc255eafc6
Oleg <kharenkoleg@gmail.com>
2023-11-19 13:48:25 +0100

    añadida la DAOException y DAOManager interface

--- DAM2_UF2_M06_A2_Premiere_League/src/main/java/DAO/DAOException.java
+++ DAM2_UF2_M06_A2_Premiere_League/src/main/java/DAO/DAOException.java
@@ -1,5 +1,20 @@
 package DAO;
 
-public class DAOException {
+public class DAOException extends Exception{
 
+	public DAOException() {
+		super();
+	}
+	
+	public DAOException(String message) {
+		super(message);
+	}
+	
+	public DAOException(Throwable cause) {
+		super(cause);
+	}
+	
+	public DAOException(String message, Throwable cause) {
+		super(message, cause);
+	}
 }
--- DAM2_UF2_M06_A2_Premiere_League/src/main/java/DAO/DAOManager.java
+++ DAM2_UF2_M06_A2_Premiere_League/src/main/java/DAO/DAOManager.java
@@ -1,5 +1,32 @@
 package DAO;
+import MODEL.Match;
+import MODEL.Team;
+import java.sql.Date;
+import java.util.ArrayList;
 
-public interface DAOManager {
+public interface DAOManager extends AutoCloseable{
 
+	public boolean AddTeam(Team oneTeam);
+	public void ImportTeams(String fileTeams);
+	public Team GetTeam(String teamAbbreviation);
+	public String GetTeamAbbreviation(String teamName);
+	public boolean AddMatch(Match oneMatch);
+	public void ImportMatches(String fileMatches);
+	public Match GetMatch(Date matchDay, Team home, Team away);
+	public int HomeGoals();
+	public int AwayGoals();
+	public ArrayList<Match> MatchesOfTeam(Team oneTeam);
+	public int RedCards(Team oneTeam);
+	public ArrayList<Team> TopRedCards();
+	
 }
+
+/*public interface EmployeeDAO extends AutoCloseable {
+
+	public void add(Employee empl);
+	public void update(Employee empl);
+	public void delete(int id);
+	public Employee findById(int id);
+	public Employee[] getAllEmployees();
+}
+*/
